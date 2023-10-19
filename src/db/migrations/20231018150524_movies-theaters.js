4e6f3338df014911cba80a60f565972ed583bbd0
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-19 10:48:53 -0400

    Adding database, correcting migrations.

--- src/db/migrations/20231018150524_movies-theaters.js
+++ src/db/migrations/20231018150524_movies-theaters.js
@@ -1,14 +1,18 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable("movies_theaters", (table) => {
+        table.integer("movie_id").unsigned().notNullable();
         table
         .foreign("movie_id")
         .references("movie_id")
-        .inTable("movies") 
+        .inTable("movies")
+        .onDelete("CASCADE"); 
+        table.integer("theater_id").unsigned().notNullable();
         table
         .foreign("theater_id")
         .references("theater_id")
         .inTable("theaters") 
+        .onDelete("CASCADE");
         table.boolean("is_showing");
       });
     };
