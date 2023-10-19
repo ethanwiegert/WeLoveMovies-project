552636bcbcffe2eb6d7c2d987b90911c86a33e50
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-19 12:12:02 -0400

    Adding imports to server structure.

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -1,7 +1,6 @@
 const service = require("./movies.service.js");
 
 async function list(req, res, next){
-    const active = req.query.is_showing;
     const data=await service.list()
     res.json({data})
 }
