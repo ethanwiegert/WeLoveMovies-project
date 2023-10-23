0e76be44f524a23d34146ae08967ee36f531a03c
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-23 12:57:53 -0400

    Completing revews requests.

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,4 +1,7 @@
 const service = require("./reviews.service.js");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+
+
 
 async function reviewExists(req, res, next){
     const { reviewId } = req.params;
@@ -10,10 +13,13 @@ async function reviewExists(req, res, next){
     return next({ status: 404, message: "Review cannot be found." });
 }
 
-async function update(req, res, next){
-    const {reviewId}=req.params
-    const data = await service.update(req.body.data, reviewId)
-    res.json({data})
+async function update(req, res) {
+  const updatedReview = { ...res.locals.review, ...req.body.data };
+  await service.update(updatedReview);
+  const reviewToReturn = await service.getReviewWithCritic(
+    res.locals.review.review_id
+  );
+  res.json({ data: reviewToReturn });
 }
 
 async function destroy(req, res, next){
@@ -23,6 +29,6 @@ async function destroy(req, res, next){
 }
 
 module.exports={
-    update:[reviewExists, update],
-    delete:[reviewExists, destroy],
+    update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
+    delete:[asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
 }
--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,4 +1,5 @@
 const knex = require("../db/connection");
+const mapProperties = require("../utils/map-properties");
 
 function read(reviewId){
     return knex("reviews")
@@ -7,12 +8,40 @@ function read(reviewId){
     .first()
 }
 
-function update(updatedReview, reviewId){
-    return knex("reviews")
-    .where({review_id:reviewId})
-    .update(updatedReview)
-    .returning("*")
-    .then((updatedRecord)=>updatedRecord[0])
+/*const update = updatedReview => {
+     return knex("reviews")
+    .select("*")
+    .where({ re: updatedReview.review_id })
+   
+};*/
+function update(updatedReview) {
+  return knex("reviews")
+    .select("*")
+    .where({ review_id: updatedReview.review_id })
+    .update(updatedReview);
+}
+
+// returnUpdate function is used to send back the updated review with nested critic object.
+// mapProperties is used to nest critic under the property, "critic".
+
+const addCriticDetails = mapProperties({
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+});
+
+
+
+function getReviewWithCritic(reviewId) {
+  return knex("reviews as r")
+    .join("critics as c", "r.critic_id", "c.critic_id")
+    .select("*")
+    .where({ review_id: reviewId })
+    .first()
+    .then((result) => {
+      const updatedReview = addCriticDetails(result)
+      return updatedReview;
+    });
 }
 
 function destroy( reviewId){
@@ -24,5 +53,6 @@ function destroy( reviewId){
 module.exports={
     read,
     update,
+    getReviewWithCritic,
     delete: destroy,
 }
