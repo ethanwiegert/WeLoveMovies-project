6954c5717674c18e801d03cf4e73afd16527d6c7
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-24 15:06:52 -0400

    Completing requests for theaters.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
 
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
@@ -13,6 +14,8 @@ app.use(express.json());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/theaters", theatersRouter)
+
 
 
 app.use(notFound);
