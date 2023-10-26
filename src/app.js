d980da7e1da2735d9df54f7a3872bdfa0115356d
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-26 15:07:57 -0400

    Moving code.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+
+
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
+
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+app.use(cors());
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader(
       "Access-Control-Allow-Origin",
@@ -23,16 +35,6 @@ app.use((req, res, next) => {
   
     next();
   });
-
-const moviesRouter = require("./movies/movies.router");
-const reviewsRouter = require("./reviews/reviews.router");
-const theatersRouter = require("./theaters/theaters.router");
-
-const notFound = require("./errors/notFound");
-const errorHandler = require("./errors/errorHandler");
-
-app.use(cors());
-app.use(express.json());
 
 app.use("/movies", moviesRouter);
 app.use("/reviews", reviewsRouter);
